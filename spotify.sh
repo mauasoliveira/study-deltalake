64e2dfa0bbfad7b7337a9d3fb422abda46c906a1
Mauricio Oliveira <mauricio.oliveira@pipefy.com>
2023-07-13 17:38:05 -0300

    Fix typo on script

--- spotify.sh
+++ spotify.sh
@@ -3,8 +3,8 @@ python3 to_parquet.py "spotify.sqlite" albums
 python3 to_parquet.py "spotify.sqlite" artists
 python3 to_parquet.py "spotify.sqlite" audio_features
 python3 to_parquet.py "spotify.sqlite" genres
-python3 to_parquet.py "spotify.sqlite" r_album_artists
-python3 to_parquet.py "spotify.sqlite" r_album_tracks
+python3 to_parquet.py "spotify.sqlite" r_albums_artists
+python3 to_parquet.py "spotify.sqlite" r_albums_tracks
 python3 to_parquet.py "spotify.sqlite" r_artist_genre
 python3 to_parquet.py "spotify.sqlite" r_track_artist
 python3 to_parquet.py "spotify.sqlite" tracks
