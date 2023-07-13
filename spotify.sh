20763e4b23a4a4cb07e4cda1b91e3f618eff9fac
Mauricio Oliveira <mauricio.oliveira@pipefy.com>
2023-07-13 17:36:11 -0300

    Added scripts to convert to Parquet

---
+++ spotify.sh
@@ -0,0 +1,10 @@
+
+python3 to_parquet.py "spotify.sqlite" albums
+python3 to_parquet.py "spotify.sqlite" artists
+python3 to_parquet.py "spotify.sqlite" audio_features
+python3 to_parquet.py "spotify.sqlite" genres
+python3 to_parquet.py "spotify.sqlite" r_album_artists
+python3 to_parquet.py "spotify.sqlite" r_album_tracks
+python3 to_parquet.py "spotify.sqlite" r_artist_genre
+python3 to_parquet.py "spotify.sqlite" r_track_artist
+python3 to_parquet.py "spotify.sqlite" tracks
